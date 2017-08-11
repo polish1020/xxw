61038c7e9a7e13184160239c6b3e2e28e5411e05
changsheng <changsheng@mogujie.com>
2017-08-11 16:05:14 +0800

    临时保存一下

---
+++ pm2.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "apps" : [{
+        "name"        : "xxw",
+        "script"      : "node",
+        "args"        : "bin/www",
+        "log_date_format": "YYYY-MM-DD HH:mm Z"
+    }]
+}
