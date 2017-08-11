26b1c4d4cbd697d053b70e188ae801add468c4c0
changsheng <changsheng@mogujie.com>
2017-08-11 14:44:45 +0800

    临时保存一下

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', {
+        title: '这是我们的小窝',
+        desc: '亲爱的徐小薇'
+    });
 });
 
 module.exports = router;
